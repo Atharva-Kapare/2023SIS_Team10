3395e2989a8c54ea67544239275c15bb2be9cdbb
Jacob Rochecouste <jrochecouste40@gmail.com>
2023-09-01 10:35:46 +1000

    express api works/change for redirect_uri:http://localhost:3000

---
+++ mood-shifter-backend/auth.js
@@ -0,0 +1,22 @@
+// var client_id = 'bf93ef9d71614b5392aa6528ba81510a';
+// var redirect_uri = 'http://localhost:3000/callback';
+
+// var app = express();
+
+// app.get('/login', function(req, res) {
+
+//   var state = generateRandomString(16);
+//   var scope = 'user-read-private user-read-email';
+
+//   res.redirect('https://accounts.spotify.com/authorize?' +
+//     querystring.stringify({
+//       response_type: 'code',
+//       client_id: client_id,
+//       scope: scope,
+//       redirect_uri: redirect_uri,
+//       state: state
+//     }));
+// });
+
+
+// module.exports = auth
--- mood-shifter-backend/index.js
+++ mood-shifter-backend/index.js
@@ -1,6 +1,28 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const auth = require('./auth')
+var querystring = require('querystring')
+
+var client_id = 'bf93ef9d71614b5392aa6528ba81510a';
+var redirect_uri = 'http://localhost:3000';
+
+app.get('/login', function(req, res) {
+
+  // var state = generateRandomString(16);
+  var state = 'bf93ef9d71614b53';
+  var scope = 'user-read-private user-read-email';
+  
+
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: client_id,
+      scope: scope,
+      redirect_uri: redirect_uri,
+      state: state
+    }));
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
