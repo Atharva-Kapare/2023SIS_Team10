e37c111560beba63dee5a83034312427f8f508bf
Sacha Gilchrist <sacha.gilchrist@gmail.com>
2023-09-06 01:43:59 +1000

    Starting framwork for collecting user data

--- mood-shifter-backend/index.js
+++ mood-shifter-backend/index.js
@@ -6,6 +6,18 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.post('/genre', (req, res) => {
+  res.send(req.body.song +  ' ' + req.body.genre)
+})
+
+app.post('/volume', (req, res) => {
+  res.send(req.body.song +  ' ' + req.body.volume)
+})
+
+app.post('/like', (req, res) => {
+  res.send(req.body.song)
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
