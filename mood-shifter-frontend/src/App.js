92d1b079e0d7ec9e689026b2d067c66092e397a1
leen-khawaja <leen.khawaja@student.uts.edu.au>
2023-09-13 00:09:03 +1000

    create play/pause buttons

--- mood-shifter-frontend/src/App.js
+++ mood-shifter-frontend/src/App.js
@@ -1,12 +1,17 @@
 import logo from './logo.svg';
+
+
 import './App.css';
 import React from 'react';
+import PlayButton from './components/playbar-buttons/play-pause';
+const rootElement = document.getElementById("root");
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -18,6 +23,9 @@ function App() {
         >
           Learn React
         </a>
+        <div>
+        </div>
+        <PlayButton  url="https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3"></PlayButton>
       </header>
     </div>
   );
--- mood-shifter-frontend/src/components/playbar-buttons/play-pause.js
+++ mood-shifter-frontend/src/components/playbar-buttons/play-pause.js
@@ -1,1 +1,77 @@
-//this .js will be the component for a button for play and pausing songs for the user
+import { FaPause, FaPlay } from 'react-icons/fa'
+import React, { Component } from "react";
+import "./styles.css";
+
+// const PlayButton = () => {
+//   return (
+//     <button className='transition opacity-0 rounded-full flex items-center bg-green-500 p-4 drop-shadow-md translate translate-y-1/4 group-hover:opacity-100 group-hover:translate-y-0 hover:scale-110'>
+//         <FaPlay className='text-black'/>
+//     </button>
+//   )
+// }
+
+// export default PlayButton
+
+class PlayButton extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playing: false
+    };
+  }
+
+  onPlay = (event) => {
+    this.setState({ playing: true });
+  };
+  onPause = (event) => {
+    this.setState({ playing: false });
+  };
+  onEnded = (event) => {
+    this.setState({ playing: false });
+  };
+
+  playAudio = () => {
+    this.audioEl.play();
+    const audio = this.audioEl;
+    audio.addEventListener("play", this.onPlay);
+    audio.addEventListener("pause", this.onPause);
+  };
+
+  pauseAudio = () => {
+    this.audioEl.pause();
+  };
+
+  startAudio = () => {
+    this.playAudio();
+  };
+
+  renderAudio = () => {
+    const { url } = this.props;
+    const { playing } = this.state;
+    const notSupportedMsg =
+      "Your browser does not support the <code>audio</code> element.";
+    return (
+      <>
+        {!playing && (
+          <i class="fa fa-play" onClick={this.startAudio}></i>
+        )}
+        {playing && <i class="fa fa-pause" onClick={this.pauseAudio}></i>}
+
+        <audio
+          src={url}
+          ref={(ref) => {
+            this.audioEl = ref;
+          }}
+        >
+          {notSupportedMsg}
+        </audio>
+      </>
+    );
+  };
+
+  render() {
+    return this.renderAudio();
+  }
+}
+
+export default PlayButton;
