8c63e9e994d69f95c0442577742b542abd6e4065
Jacob Rochecouste <jrochecouste40@gmail.com>
2023-10-03 19:43:14 +1100

    Select mood and tag song pages UI components created

--- mood-shifter-frontend/src/components/getting-started/getting-started.js
+++ mood-shifter-frontend/src/components/getting-started/getting-started.js
@@ -2,6 +2,8 @@ import './getting-started.css';
 import React from 'react';
 import spotifyLogo from '../../assets/spotify-logo.png';
 
+const tagCount = localStorage.getItem('tagCount');
+
 function GettingStartedScreen( { navigation } ) {
     return (
         <div className="login">
@@ -9,7 +11,15 @@ function GettingStartedScreen( { navigation } ) {
             <div className="login-content-div">
                 <h1 className="heading">Welcome to Moodshifter!</h1>
                 <p className="call-to-action-text">Let's personalise your song choices to give you the best experience.</p>
-                <button className="sign-in-button-style" onClick={() => navigation.navigate('SelectMoodScreen')}>Get Started</button>
+                <button className="sign-in-button-style" 
+                    onClick={() => { 
+                        if(tagCount >= 3) {
+                            navigation.navigate('PlaylistScreen');
+                        } else {
+                            navigation.navigate('SelectMoodScreen');
+                        }
+                    }}
+                >Get Started</button>
             </div>
             <img src={spotifyLogo} alt="Spotify Logo" className="spotify-logo-style"></img>
         </div>
--- mood-shifter-frontend/src/components/getting-started/select-mood.js
+++ mood-shifter-frontend/src/components/getting-started/select-mood.js
@@ -4,11 +4,40 @@ import './getting-started.css';
 function SelectMoodScreen( { navigation } ) {
     return (
         <div className="login">
-            <h1 className="heading">Select Mood Screen</h1>
+            <div className="header-div">
+                <h4 className="text-style step-text">Step 1</h4>
+                <h1 className="text-style">Select Mood</h1>
+                <h3 className="text-style sub-text">Choose a common mood you find yourself in</h3>
+                <button className='mood-button-style new-mood-button-style'><p className="mood-item new-mood-item">+</p></button>
+            </div>
+
+            
+            
+            {/* Mood List */}
+            <div className='mood-div'>
+                <h3 className="section-header">Suggested</h3>
+                <div className='mood-row'>
+                    <button className='mood-button-style'><p className="mood-item">Happy</p></button>
+                    <button className='mood-button-style'><p className="mood-item">Sleepy</p></button>
+                    <button className='mood-button-style'><p className="mood-item">Stressed</p></button>
+                </div>
+                <div className='mood-row'>
+                    <button className='mood-button-style'><p className="mood-item">Tired</p></button>
+                    <button className='mood-button-style'><p className="mood-item">Annoyed</p></button>
+                    <button className='mood-button-style'><p className="mood-item">Sad</p></button>
+                </div>
+                <div className='mood-row'>
+                    <button className='mood-button-style'><p className="mood-item">Jealous</p></button>
+                    <button className='mood-button-style'><p className="mood-item">Enthusiastic</p></button>
+                    <button className='mood-button-style'><p className="mood-item">Anxious</p></button>
+                    <button className='mood-button-style'><p className="mood-item">Surprised</p></button>
+                </div>
+            </div>
+            {/* Mood List */}
+
             <button className="sign-in-button-style" onClick={() => navigation.navigate('TagSongsScreen')}>Continue</button>
         </div>
     );
 }
-
 
 export default SelectMoodScreen;
