7657599710d822a19dc8535d61e687cde4f22d2d
jrochecouste <jacob.rochecouste@student.uts.edu.au>
2023-10-05 12:25:28 +1100

    adding functionality to GettingStarted Components

--- mood-shifter-frontend/src/components/getting-started/congratulations.js
+++ mood-shifter-frontend/src/components/getting-started/congratulations.js
@@ -4,8 +4,12 @@ import './getting-started.css';
 function SelectMoodScreen( { navigation } ) {
     return (
         <div className="login">
-            <h1 className="heading">Congratulations</h1>
-            <button className="sign-in-button-style" onClick={() => navigation.navigate('PlaylistScreen')}>Continue</button>
+            <div className="congrats-div">
+                <h1 className="congrats-heading">Congratulations</h1>
+                <p className="text-style congrats-text-style">We are collating your song choices and generating a personalised experience to provide you with the best playlist mixes.</p>
+                <button className="sign-in-button-style" onClick={() => navigation.navigate('PlaylistScreen')}>Tag More Moods</button>
+                <button className="finish-button-style" onClick={() => navigation.navigate('PlaylistScreen')}>Finish</button>
+            </div>
         </div>
     );
 }
