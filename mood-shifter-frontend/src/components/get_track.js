5e7156b8d6aa2ea0d2c6a1e8a090e991d5d08623
Uddhav Parashar <uddhavparashar6@gmail.com>
2023-10-11 11:44:55 +1100

    got an array returned

--- mood-shifter-frontend/src/components/get_track.js
+++ mood-shifter-frontend/src/components/get_track.js
@@ -1,13 +1,24 @@
 import React from "react";
 import authentication from "../authentication";
 
-const trackID = '11dFghVXANMlKmJXsNCbNl'; 
+const trackID = '1S7rX5dnPYrlMCJ3DBvlJn'; 
+let trackInfo = []
+//const trackInfo = authentication.getSong(trackID).then(data => {console.log(data)})
 
 function GetTrack(){
     return(
-    <h1>test</h1>,
-   console.log(authentication.getSong('1S7rX5dnPYrlMCJ3DBvlJn'))
 
+     authentication.getSong(trackID).then(data => {
+        console.log(data)
+    }), 
+    <div>
+        <h1>test</h1>
+
+    </div>
+
+
+
+    //console.log(authentication.getSong('6xU0ojuUR3dAhbnrJPQr1I'))    
     ); 
 }
 
