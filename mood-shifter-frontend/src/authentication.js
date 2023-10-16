2825f320697c73a20dde1b136bd1484bddfd4373
leen-khawaja <leen.khawaja@student.uts.edu.au>
2023-10-16 20:19:50 +1100

    functionality for song player page

--- mood-shifter-frontend/src/authentication.js
+++ mood-shifter-frontend/src/authentication.js
@@ -8,7 +8,7 @@ export async function redirectToAuthCodeFlow(clientId) {
     params.append("client_id", clientId);
     params.append("response_type", "code");
     params.append("redirect_uri", "http://localhost:3000");
-    params.append("scope", "user-read-private user-read-email playlist-modify-private user-library-read");
+    params.append("scope", "user-read-private user-read-email playlist-modify-private user-library-read app-remote-control streaming");
     params.append("code_challenge_method", "S256");
     params.append("code_challenge", challenge);
     document.location = `https://accounts.spotify.com/authorize?${params.toString()}`;
--- mood-shifter-frontend/src/components/song-player/playbar-buttons/song-details.js
+++ mood-shifter-frontend/src/components/song-player/playbar-buttons/song-details.js
@@ -1,14 +1,14 @@
 import '../song-player.css';
 import React, { Component } from "react";
 
-const SongDetails = ({ song }) => {
+const SongDetails = ({ track }) => {
     return (
         <>
-        <img className="playbar-album-cover" src={song.albumCover} alt={song.title} />
+        <img className="playbar-album-cover" src={track?.image ?? ''} />
         <div className="playbar-info">
             <div className="playbar-song-info">
-                <p className="playbar-song-title">{song.title}</p>
-                <p className="playbar-song-artist">{song.artist}</p>
+                <p className="playbar-song-title">{track?.name ?? ''}</p>
+                <p className="playbar-song-artist">{track?.artists[0]?.name ?? ''}</p>
             </div>
         </div></>
     );
