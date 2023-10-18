4c2c9ce9705e55bc47abcad0caf3f4359cf08590
Atharva Kapare <atharva.s.kapare@gmail.com>
2023-10-18 14:57:44 +1100

    More bugs

--- mood-shifter-backend/index.js
+++ mood-shifter-backend/index.js
@@ -11,7 +11,7 @@ import { database } from './firebase_init.js';
 
 import { doc, getDoc, setDoc } from "firebase/firestore";
 import axios from 'axios';
-// import { getLikedSongs } from './spotify.js';
+import { getLikedSongs } from './spotify.js';
 
 var client_id = 'bf93ef9d71614b5392aa6528ba81510a';
 var redirect_uri = 'http://localhost:3000';
@@ -22,50 +22,58 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.post('/login', function (req, res) {
+app.post('/login', async function (req, res) {
   // We get sent the access token and the userID from the frontend
   // The access token is needed to talk to the spotify apis, userID needs to get stored into the backend
   let resp = {};
 
-  console.log(JSON.stringify(req.body));
+  // console.log(JSON.stringify(req.body));
 
   // Get all of the liked songs from the user:
   // var likedSongs = await axios.
 
   // Get the user data if it exists from firebase
   const docRef = doc(database, "users", req.body.UID);
-  const docSnapshot = getDoc(docRef);
+  // const docSnapshot = await getDoc(docRef);
 
-  if (docSnapshot != null) {
-    resp.gettingStarted = false;
+  const docSnapshot = await getDoc(docRef);
+  // console.log(docSnapshot);
+  const document = docSnapshot.data();
+
+  console.log("Document: ", document);
+
+
+
+  if (document != undefined) {
+    resp.gettingStarted = document.gettingStarted;
   } else {
-
-    if (docSnapshot.gettingStarted == false) {
-      resp.gettingStarted = false;
-    }
 
     resp.gettingStarted = true;
     setDoc(doc(database, 'users', req.body.UID), {
       "moods": {},
       "model": "",
-      "configs": {}
-    });
-    console.log("User with ID: ", req.body.UID, "was made!")
+      "configs": {},
+      "gettingStarted": true
+    }).then(console.log("User with ID: ", req.body.UID, "was made!"));
+    
   }
   
 
   // Talk to the spotify apis to grab the liked songs
-  resp.likedSongs = [];
-  getLikedSongs(req.body.accessToken)
-  .then(spotifyResp => {
-    // console.log(spotifyResp.items);
-    for(const item in spotifyResp.items) {
-      resp.likedSongs.push(spotifyResp.items[item].track.id);
-      // resp.likedSongs.push(track.id);
-    }
-    console.log(resp);
-    res.send(resp);
-  });
+  // resp.likedSongs = [];
+  const spotifyResp = await getLikedSongs(req.body.accessToken);
+  let likedSongs = [];
+  // console.log(spotifyResp);
+  for(const item in spotifyResp.items) {
+    likedSongs.push(spotifyResp.items[item].track.id);
+    // console.log(spotifyResp.items[item].track.id);
+    // resp.likedSongs.push(track.id);
+  }
+
+  resp.likedSongs = likedSongs;
+
+  console.log(resp);
+  res.send(resp);
 });
 
 app.get('/', (req, res) => {
