4440bd5b22779e7f91692e3112b6bf489603f6aa
Atharva Kapare <atharva.s.kapare@gmail.com>
2023-10-24 21:32:12 +1100

    New fix

--- mood-shifter-backend/index.js
+++ mood-shifter-backend/index.js
@@ -9,7 +9,7 @@ const port = 8000
 import { firebase_app } from './firebase_init.js';
 import { database } from './firebase_init.js';
 
-import { doc, getDoc, setDoc } from "firebase/firestore";
+import { doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import axios from 'axios';
 import { getLikedSongs } from './spotify.js';
 
@@ -27,62 +27,86 @@ app.post('/login', async function (req, res) {
   // We get sent the access token and the userID from the frontend
   // The access token is needed to talk to the spotify apis, userID needs to get stored into the backend
   let resp = {};
-  // console.log(JSON.stringify(req.body));
-  
-  // Get all of the liked songs from the user:
-  // var likedSongs = await axios.
   
   // Get the user data if it exists from firebase
-  console.log(req)
   const docRef = doc(database, "users", req.body.UID);
-  console.log("TESTTESTTESTTESTTEST")
-  // const docSnapshot = await getDoc(docRef);
 
   const docSnapshot = await getDoc(docRef);
-  // console.log(docSnapshot);
-  const document = docSnapshot.data();
+  // Need to check if the doc snapshot exists or not, if not, need to create doc
 
-  console.log("Document: ", document);
-
-  if (document != undefined) {
+  if (docSnapshot.exists()){
+    var document = docSnapshot.data();
     resp.gettingStarted = document.gettingStarted;
   } else {
-
     resp.gettingStarted = true;
-    setDoc(doc(database, 'users', req.body.UID), {
-      "moods": {},
-      "model": "",
-      "configs": {},
-      "gettingStarted": true
-    }).then(console.log("User with ID: ", req.body.UID, "was made!"));
-    
   }
+
+  // console.log("Document: ", document);
   
   // Talk to the spotify apis to grab the liked songs
   // resp.likedSongs = [];
-  const spotifyResp = await getLikedSongs(req.body.accessToken);
-  let likedSongs = [];
-  // console.log(spotifyResp);
-  for(const item in spotifyResp.items) {
-    likedSongs.push(spotifyResp.items[item].track.id);
-    // console.log(spotifyResp.items[item].track.id);
-    // resp.likedSongs.push(track.id);
-  }
+  let likedSongs = await getLikedSongs(req.body.accessToken);
+  
+  // likedSongs = likedSongs.splice(0,20);
+  // let likedSongs = [];
+  // // console.log(spotifyResp);
+  // for(const item in spotifyResp.items) {
+  //   likedSongs.push(spotifyResp.items[item].track.id);
+  //   // console.log(spotifyResp.items[item].track.id);
+  //   // resp.likedSongs.push(track.id);
+  // }
 
   resp.likedSongs = likedSongs;
 
   console.log("Resp: ", resp);
   res.send(resp);
+
+  // Now we need to take the liked songs and send it to the model so it can give us back the model to store in firebase
+
+  // //UNCOMMENT BELOW
+  // const modelResp = await fetch(modelURLBase + "/new_user", {
+  //   method: "POST", 
+  //   body: JSON.stringify({"likedSongs": likedSongs}),
+  //   headers: {
+  //     "Content-Type": "application/json"
+  //   }
+  // });
+
+  // console.log("ModelResp: ", modelResp);
+  // let response = await modelResp.json();
+  // console.log("Res: ", response)
+
+  setDoc(doc(database, 'users', req.body.UID), {
+    "moods": {},
+    // "model": response,
+    "configs": {},
+    "gettingStarted": docSnapshot.exists() ? document.gettingStarted : true
+  }).then(console.log("Document for: ", req.body.UID, "was set"));
+
 });
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
-app.post('/gettingStarted', (req, res) => {
-  res.send('Getting Started Works!')
-})
+app.post('/gettingStarted', async (req, res) => {
 
+  const docRef = doc(database, "users", req.body.UID);
+  const docSnapshot = await getDoc(docRef);
+
+  console.log(docSnapshot.exists());
+  if (!docSnapshot.exists()) {
+    res.send("The user does not exist.");
+  } else {
+    // setDoc(docRef.gettingStarted, false);
+    // const resp = await docRef.update({gettingStarted: "false"});
+    updateDoc(docRef, {gettingStarted: false})
+    .then(() => {
+      res.send(`Set the getting started flag to False for user: ${req.body.UID}`);
+    });
+  }
+
+})
 
 app.get('/test', (req, res) => {
   const resp = setDoc(doc(database, "cities", "LA"), {
