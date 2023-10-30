eb1e251b87d5de9e2f9789dcda599509e134f6a5
alexiabayyouk <alexiabayyouk@yahoo.com>
2023-10-31 01:17:26 +1100

    made select mood n tag songs dynamic w css changes

--- mood-shifter-frontend/src/components/getting-started/select-mood.js
+++ mood-shifter-frontend/src/components/getting-started/select-mood.js
@@ -17,59 +17,61 @@ function SelectMoodScreen( { route, navigation } ) {
     console.log("USESTATELIST: ", updatedList)
 
     return (
+        <div className='App-header'>
         <Box>
             <Grid container rowSpacing={{ xs: 2, sm: 3, md: 4 }} columnSpacing={{ xs: 2, sm: 3, md: 4 }} columns={{ xs: 2, sm: 6, md: 12 }}>
-                <div className="login">
-                    <div className="header-div">
-                        <h4 className="text-style step-text">Step 1</h4>
-                        <h1 className="text-style">Select Mood</h1>
-                        <h3 className="text-style sub-text">Choose a common mood you find yourself in</h3>
-                        <button className='mood-button-style new-mood-button-style' onClick={() => navigation.navigate("NewMoodScreen")}>+</button>
-                    </div>
-
-                    {/* Mood List */}
-                    <div className='mood-div'>
-                        <h3 className="section-header">Suggested</h3>
-                        <div className='mood-row'>
-                            {updatedList.moods.map(mood => (
-                                <Grid item xs={1} sm={2} md={3}>
-                                    <div>
-                                        <button className='mood-button-style' onClick={() => changeSelected(mood)}>{mood}</button>
-                                    </div>
-                                </Grid>
-                            ))}
-                        </div>
-                    </div>
-                    {/* Mood List */}
-
+                <Grid item xs={2} sm={6} md={12}>
+                    <h4 className="text-style step-text">Step 1</h4>
+                </Grid>
+                <Grid item xs={2} sm={6} md={12}>
+                    <h1 className="text-style">Select Mood</h1>
+                </Grid>
+                <Grid item xs={2} sm={6} md={12}>
+                    <h3 className="text-style section-header">Choose a common mood you find yourself in</h3>
+                </Grid>
+                <Grid item xs={2} sm={6} md={12}>
+                    <button className='mood-button-style new-mood-button-style' onClick={() => navigation.navigate("NewMoodScreen")}>+</button>
+                </Grid>
+                <Grid item xs={2} sm={6} md={12}>
+                    <h3 className="section-header">Suggested</h3>
+                </Grid>
+                <Grid item xs={2} sm={6} md={12}>
+                    {updatedList.moods.map(mood => (
+                        <button className='mood-button-style' onClick={() => changeSelected(mood)}>{mood}</button>
+                    ))}
+                </Grid>
+                
+                
+                <Grid item xs={2} sm={6} md={12}>
                     <button className="sign-in-button-style" onClick={() => 
-                        { 
-                            if(selected === 'none') {
-                                alert("You must select a mood to continue!");
-                            } else {
-                                navigation.push('TagSongsScreen', 
-                                    {
-                                        selectedMood: selected,
-                                    }
-                                );
+                            { 
+                                if(selected === 'none') {
+                                    alert("You must select a mood to continue!");
+                                } else {
+                                    navigation.push('TagSongsScreen', 
+                                        {
+                                            selectedMood: selected,
+                                        }
+                                    );
 
 
 
-                                let result = {"moods":[]};
-                                updatedList.moods.forEach(mood => {
-                                    if(mood !== selected) {
-                                        result.moods.push(mood);
-                                    }
-                                    return result
-                                })
-                                setUpdatedList(result);
-                                selected = 'none';
-                            }
-                        }}
-                    >Continue</button>
-                </div>
+                                    let result = {"moods":[]};
+                                    updatedList.moods.forEach(mood => {
+                                        if(mood !== selected) {
+                                            result.moods.push(mood);
+                                        }
+                                        return result
+                                    })
+                                    setUpdatedList(result);
+                                    selected = 'none';
+                                }
+                            }}
+                        >Continue</button>
+                </Grid>
             </Grid>
         </Box>
+        </div>
     );
 }
 
